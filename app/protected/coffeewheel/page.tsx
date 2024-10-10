947de4214afcdea23ee2cce833c9db37c375ebc1
Benjamin Tran <138626394+BenTran-Personal@users.noreply.github.com>
2024-10-09 19:30:35 -0700

    Start of Feedback

---
+++ app/protected/coffeewheel/page.tsx
@@ -0,0 +1,10 @@
+import CoffeeWheel from '@/components/CoffeeWheel';
+
+export default function GooglePlaces() {
+  return (
+    <div>
+      <h1>Coffee Feedback</h1>
+      <CoffeeWheel />
+    </div>
+  );
+}
