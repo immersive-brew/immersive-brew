6a0594a0dfac6a4cc27f5dad2fcdefd79e0aa23b
Jerome-0912 <120338556+Jerome-0912@users.noreply.github.com>
2024-12-14 10:58:59 -0800

    Update AuthButton.tsx

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -5,9 +5,14 @@ import { redirect } from "next/navigation";
 export default async function AuthButton() {
   const supabase = createClient();
 
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
+  const name = user ? user.user_metadata.full_name : null;
+
+
 
   const signOut = async () => {
     "use server";
@@ -19,7 +24,7 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {name}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
